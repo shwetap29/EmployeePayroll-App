de599c3380c2062ad3b0e5acaddb5b776f3f63a9
shwetap29 <shwetaprasad48@gmail.com>
2021-07-12 12:45:41 +0530

    ADDED:event listner in salary range to display value

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,3 +1,5 @@
+//UC8 Set the EVENT listner in salary range
+ 
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
