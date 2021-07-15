63c04320df3299b0b9503a36b4fd18bbb9aa795e
shwetap29 <shwetaprasad48@gmail.com>
2021-07-15 10:26:01 +0530

    ADDED:Functionality To remove an element from Payroll details

--- js/home.js
+++ js/home.js
@@ -6,14 +6,10 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 const getEmployeePayrollDataFromStorage = () => {
-    return localStorage.getItem('EmployeePayrollList') ? 
-                    JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
 }
 
 const createInnerHtml = () => {
-    
-    if(employeePayrollList.length == 0 ) return;
-
     const headerHtml = `<tr>
     <th></th>
     <th>Name</th>
@@ -23,7 +19,7 @@ const createInnerHtml = () => {
     <th>Start Date</th>
     <th>Actions</th>
     </tr>`;
-  
+  if(employeePayrollList.length==0) return;
     let innerHtml = `${headerHtml}`;
     for(const employeePayrollData of employeePayrollList){
         innerHtml = `${innerHtml}
@@ -35,9 +31,9 @@ const createInnerHtml = () => {
             <td>${employeePayrollData._salary}</td>
             <td>${employeePayrollData._startDate}</td>
             <td>
-                <img name="${employeePayrollData._id}" onclick="remove()" alt="delete" 
+                <img id="${employeePayrollData._id}" onclick="remove(this)" alt="delete" 
                 src="../assets/icons/delete-black-18dp.svg">
-                <img name="${employeePayrollData._id}" onclick="update()" alt="edit" 
+                <img id="${employeePayrollData._id}" onclick="update(this)" alt="edit" 
                 src="../assets/icons/create-black-18dp.svg">
             </td>
     </tr>`;
@@ -53,11 +49,16 @@ const getDeptHtml = (deptList) => {
     return deptHtml;
 }
 
-const formatDate = (date) => {
-    let startDate = new Date(date);
-    const options = {
-        year: 'numeric', month: 'long', day: 'numeric'
-    };
-    const empDate = !startDate ? "undefined" : startDate.toLocaleDateString("en-IN", options);
-    return empDate;
+const remove = (node) => {
+    let employeePayrollData = employeePayrollList.find(emp => emp._id == node.id);
+    if(!employeePayrollData) return;
+    const index = employeePayrollList
+                                    .map(emp => emp._id)
+                                    .indexOf(employeePayrollData._id);
+    employeePayrollList.splice(index,1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
+    createInnerHtml();
 }
+
+localStorage.removeItem("id");
